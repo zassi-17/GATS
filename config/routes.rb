34c175c16da10094465490fd9ce16b0a390b0179
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-10-03 08:11:16 +0000

    top/aboutページ機能完了（レイアウトは後で）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :members
+  devise_for :admin
+
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
