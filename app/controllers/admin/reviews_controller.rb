c591a0805ed3ad4864f6a6b64fa584d0c8d9b4c0
Ubuntu <sgk.k.s.17@gmail.com>
2025-10-20 18:30:41 +0000

    レビュー一覧・詳細ページ実装、レビューコメントコントローラー作成

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -5,31 +5,15 @@ class Admin::ReviewsController < ApplicationController
     @reviews = Review.page(params[:page])
   end
 
-  def edit
+  def show
     @review = Review.find(params[:id])
-  end
-
-  def update
-    @review = Review.find(params[:id])
-    if @review.update(review_params)
-      redirect_to admin_reviews_path
-    else
-      render "edit"
-    end
+    @member = @review.member
   end
 
   def destroy
     review = Review.find(params[:id])
     review.destroy
     redirect_to admin_reviews_path
-  end
-
-
-  private
-
-  #ストロングパラメータ
-  def review_params
-    params.require(:review).permit(:title, :body, :genre_id, :is_active, :rating)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
 
     resources :genres, except: [:new, :show]
 
-    resources :reviews, except: [:new, :show]
+    resources :reviews, except: [:new, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
