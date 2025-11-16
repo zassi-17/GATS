f644873dcb0cde5b9b0f42467ed36590af6a877d
Ubuntu <sgk.k.s.17@gmail.com>
2025-11-16 18:59:31 +0000

    繰り返し記述したparamsを編集、コメントアウト記述

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,16 +5,19 @@ class Public::SearchesController < ApplicationController
     @model = params[:model]
     @key_word = params[:key_word]
 
+    #検索時、モデルタブで会員かレビュータイトルどちらを選んだか判定して検索する条件文
     if @model == "Member"
-      @members = Member.looks(params[:search],params[:key_word]).where(is_active: true)
+      @members = Member.looks(params[:search],@key_word).where(is_active: true)
                    .where.not(email: Member::GUEST_MEMBER_EMAIL).page(params[:page])
     else
-      @reviews = Review.looks(params[:search],params[:key_word]).where(is_active: true).page(params[:page])
+      @reviews = Review.looks(params[:search],@key_word).where(is_active: true).page(params[:page])
     end
   end
 
   def genre_search
     @genre = Genre.find_by(id: params[:genre_id])
+
+    #ジャンル検索タブを選ばず検索を押下された場合のエラー回避のための条件文
     if @genre.nil?
       @reviews = Review.where(is_active: true).page(params[:page])
     else
